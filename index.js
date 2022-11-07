64bfd606af8f051f8494d236e2105901800568b8
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-07 15:39:46 -0600

    added a function allowing the user to add an employee

--- index.js
+++ index.js
@@ -319,3 +319,71 @@ function addDepartment() {
       });
     console.log(13);
 }
+
+function addEmployee() {
+    inquirer
+      .prompt([
+        {
+          name: "first_name",
+          message: "Hey a new hire. What's there name?",
+        },
+      ])
+      .then((res) => {
+        let firstName = res.first_name;
+        let lastName = res.last_name;
+  
+        db.findAllRoles().then(([rows]) => {
+          let roles = rows;
+          const roleChoices = roles.map(({ id, title }) => ({
+            name: title,
+            value: id,
+          }));
+  
+          inquirer
+            .prompt({
+              type: "list",
+              name: "roleId",
+              message: "What role is the new hire filling Boss?",
+              choices: roleChoices,
+            })
+            .then((res) => {
+              let roleId = res.roleId;
+  
+              db.findAllEmployees().then(([rows]) => {
+                let employees = rows;
+                const managerChoices = employees.map(
+                  ({ id, first_name, last_name }) => ({
+                    name: `${first_name} ${last_name}`,
+                    value: id,
+                  })
+                );
+  
+                managerChoices.unshift({ name: "None", value: null });
+  
+                inquirer
+                  .prompt({
+                    type: "list",
+                    name: "managerId",
+                    message: "Who is the new hire's manager?",
+                    choices: managerChoices,
+                  })
+                  .then((res) => {
+                    let employee = {
+                      manager_id: res.managerId,
+                      role_id: roleId,
+                      first_name: firstName,
+                      last_name: lastName,
+                    };
+  
+                    db.createEmployee(employee);
+                  })
+                  .then(() =>
+                    console.log(`Added ${firstName} ${lastName} to the database`)
+                  )
+                  .then(() => runPrompts());
+              });
+            });
+        });
+      });
+    console.log(16);
+}
