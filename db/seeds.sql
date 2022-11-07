2ea0d3a44de8e8c1a7ca4b75f997841751b7aafb
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-07 15:05:25 -0600

    added roles and values to the seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,15 @@ VALUES
     ('Engineering'),
     ('Finance'),
     ('Legal');
+    
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Head of Sales', 90000, 1),
+    ('Sales Member', 75000, 1),
+    ('Head Engineering', 150000, 2),
+    ('Software Engineer', 100000, 2),
+    ('Head of Accounting', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Head of the Legal Team', 250000, 4),
+    ('Lawyer', 200000, 4);
