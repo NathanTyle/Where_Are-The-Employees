eeeecd73965c3d3858e5a8b85d7ae870a423d371
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-07 15:05:58 -0600

    added emploees and values to the seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,3 +19,15 @@ VALUES
     ('Accountant', 125000, 3),
     ('Head of the Legal Team', 250000, 4),
     ('Lawyer', 200000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Spike', 'Speagle', 1, NULL),
+    ('Char', 'Aznable', 2, 1),
+    ('Van', 'Flyheight', 3, NULL),
+    ('Motoko', 'Kusanagi', 4, 3),
+    ('Kamille', 'Bidan', 5, NULL),
+    ('Mary', 'Brown', 6, 5),
+    ('Brittany', 'Sebbane', 7, NULL),
+    ('Victor', 'Santos', 8, 7);
