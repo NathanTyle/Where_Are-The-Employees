06bbc74c5cc1670087ec7118508d90e21ca9504f
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-07 15:26:54 -0600

    added init to start programs

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const logo = require("asciiart-logo");
 const mysql = require("mysql2");
 require("console.table");
 const db = require("./db");
+
+init();
+
+function init() {
+  const logoText = logo({ name: "El Jefe" }).render();
+
+  console.log(logoText);
+
+  runPrompts();
+  console.log(1);
+}
